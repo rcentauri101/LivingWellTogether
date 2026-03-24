28155547825f0cbacf5d49f31666151a979c1e0a
rcentauri101 <39754905+rcentauri101@users.noreply.github.com>
2026-03-23 19:53:24 -0700

    Update Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://livingwelltogether.github.io",
+  site: 'https://rcentauri101.github.io',
+  base: '/LivingWellTogether',
   output: "static"
 });
